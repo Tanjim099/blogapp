ddf2d93f9eaa8240ec392c719ab0a87725c6ec52
Tanjim <tanjimseo@gmail.com>
2023-10-18 11:22:47 +0530

    Added user delete route and controller

--- server/controller/miscellaneousController.js
+++ server/controller/miscellaneousController.js
@@ -3,14 +3,12 @@ const { userModel } = require("../models/userModel")
 
 const userStats = async (req, res) => {
     const allUsersCount = await userModel.countDocuments();
-    const allUsers = await userModel.find()
     const allBlogsCount = await blogModel.countDocuments();
     res.status(200).send({
         success: true,
         message: "getted all user and blog",
         allUsersCount,
-        allBlogsCount,
-        allUsers
+        allBlogsCount
     })
 }
 
@@ -29,4 +27,26 @@ const getAllUsers = async (req, res) => {
     }
 }
 
-module.exports = { userStats, getAllUsers }
+const deleteUser = async (req, res) => {
+    const { userId } = req.params;
+    console.log(userId)
+    try {
+        if (!userId) {
+            return res.status(401).send({
+                message: "id is not exist"
+            })
+        }
+        const user = await userModel.findByIdAndDelete(userId);
+        res.status(200).send({
+            success: true,
+            message: "User deleted successfully",
+            user
+        })
+    } catch (error) {
+        res.status(501).send({
+            message: error.message
+        })
+    }
+}
+
+module.exports = { userStats, getAllUsers, deleteUser }
--- server/routes/miscellaneousRoute.js
+++ server/routes/miscellaneousRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const { userStats, getAllUsers } = require("../controller/miscellaneousController");
+const { userStats, getAllUsers, deleteUser } = require("../controller/miscellaneousController");
 
 const miscRoutes = express.Router();
 
 miscRoutes.get("/stats/users", userStats);
-miscRoutes.get("/stats/allusers", getAllUsers)
+miscRoutes.get("/stats/allusers", getAllUsers);
+miscRoutes.delete("/stats/user/delete/:userId", deleteUser);
 
 module.exports = { miscRoutes }
